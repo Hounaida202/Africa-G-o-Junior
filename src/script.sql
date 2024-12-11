4e1adaa694688207768148220dc1117db61f6f27
Hounaida202 <etrichih@gmail.com>
2024-12-11 17:05:15 +0100

    Insérer dans la table des pays

--- src/script.sql
+++ src/script.sql
@@ -28,3 +28,27 @@ CREATE TABLE ville
 
 INSERT INTO continent (`continent_id`, `nom`) VALUES
 (1, 'Africa');
+
+INSERT INTO pays (`pays_id`, `nom`, `langue`, `population`, `continent_id`) 
+VALUES
+(1, 'Maroc', 'Arabe', 37460000, 1),
+(2, 'Afrique Du sud', 'Anglais', 59890000, 1),
+(3, 'Egypte', 'Arabe', 100990000, 1),
+(4, 'Sénégale', 'Français', 17320000, 1),
+(5, 'Kenya', 'Anglais', 54030000, 1),
+(6, 'Madagascar', 'Français', 29610000, 1),
+(7, 'Sudan', 'Arabe', 46870000, 1),
+(8, 'Nigeria', 'Anglais', 218540000, 1),
+(9, 'Ethiopia', 'Amharique', 126500000, 1),
+(10, 'Sudan', 'Arabe', 46870000, 1),
+(11, 'Ghana', 'Anglais', 32940000, 1), 
+(12, 'Tanzania', 'Swahili', 65890000, 1), 
+(13, 'Somalia', 'Somali', 18000000, 1),
+(14, 'Angola', 'Portugais', 36550000, 1),
+(15, 'Cameroon', 'Français', 28000000, 1),
+(16, 'Ivory Coast', 'Français', 27770000, 1),
+(17, 'Uganda', 'Anglais', 47790000, 1),
+(18, 'Zimbabwe', 'Anglais', 16500000, 1),
+(19, 'Libya', 'Arabe', 6900000, 1),
+(20, 'Chad', 'Arabe', 18300000, 1)
+;
