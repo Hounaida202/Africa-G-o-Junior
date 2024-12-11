940cbd78baf0e5e154d72ebb2d6645d6db8c82c6
Hounaida202 <etrichih@gmail.com>
2024-12-11 17:01:05 +0100

    Créer une table pour ville

--- src/script.sql
+++ src/script.sql
@@ -5,3 +5,14 @@ CREATE TABLE continent
   PRIMARY KEY(continent_id)
 );
 
+CREATE TABLE pays
+(
+  pays_id INT,
+  nom VARCHAR(255),
+  langue VARCHAR(255),
+  population INT,
+  continent_id INT,
+  PRIMARY KEY (pays_id),
+  FOREIGN KEY(continent_id) REFERENCES continent(continent_id)
+  
+);
